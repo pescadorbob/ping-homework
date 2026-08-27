ca174682918e92a64f1c1fa47338f09a7e23cbc9
Brent Fisher <bc.fisher@yahoo.com>
2026-08-26 21:54:17 -0600

    refactored to parameterized test for LogicalOperator for multiple tests

--- src/test/java/com/brent/ping/LogicalOperatorShould.java
+++ src/test/java/com/brent/ping/LogicalOperatorShould.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import static com.brent.ping.ResourceTestBuilder.aResource;
@@ -12,21 +13,31 @@ import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class LogicalOperatorShould {
 
-    @Test    
-    void testLogicalOperator(){
+    @ParameterizedTest(name = " {0} when x={1} applied to {2}")
+    @MethodSource("testCases")
+    void testLogicalOperator(String lhsProperty, Boolean lhsValue,
+        String rhsProperty, Boolean rhsValue, LogicalOperatorType operatorType, 
+        String resourceProperty1, String resourceValue1,
+        String resourceProperty2, String resourceValue2,
+        Boolean expectedResult, String description
+    ){
         
-        var lhs = new BooleanExpression("isActive", true);
-        var rhs = new BooleanExpression("isEnabled", false);
-        var logicalOperator = new LogicalOperator(LogicalOperatorType.AND);
+        var lhs = new BooleanExpression(lhsProperty, lhsValue);
+        var rhs = new BooleanExpression(rhsProperty, rhsValue);
+        var logicalOperator = new LogicalOperator(operatorType);
         var binomialExpression = new BinomialExpression(lhs, logicalOperator, rhs);
-        var resource = aResource().withProperty("isActive","true").withProperty("isEnabled","false").build();
+        var resource = aResource().withProperty(resourceProperty1,resourceValue1).withProperty(resourceProperty2,resourceValue2).build();
         var filter = new Filter(binomialExpression);
-        var expectedResult = true;
 
         var actual = filter.matches(resource);
 
-        assertThat(actual).as("isActive AND not isEnabled").isEqualTo(expectedResult);
+        assertThat(actual).as(description).isEqualTo(expectedResult);
     }
-
+    private static Stream<Arguments> testCases() {
+        return Stream.of(
+                arguments("isActive", true, "isEnabled", false, LogicalOperatorType.AND, 
+                "isActive","true","isEnabled","false",true,"isActive AND not isEnabled")
+        );
+    }
     
 }
